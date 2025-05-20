a9e7e95da4fc2bbab8a0d3d53ee1fab1c342c585
matcha20070516 <ma0516_hogreen@icloud.com>
2025-05-20 23:43:17 +0900

    Update exresult.js

--- exresult.js
+++ exresult.js
@@ -2,7 +2,6 @@
 // ページ読み込み時に実行
 window.addEventListener("DOMContentLoaded", () => {
   // localStorageから取得（デフォルト値も指定）
-  const username = localStorage.getItem("exUsername") || "名無し";
   const score = localStorage.getItem("exScore") || "0";
   const setName = localStorage.getItem("exSetName") || "謎検模試セット";
   const attemptCount = localStorage.getItem("exAttemptCount") || "1";
