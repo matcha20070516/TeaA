889834e31880ca6ab6090cbdc8dad9ce9bf0f173
matcha20070516 <ma0516_hogreen@icloud.com>
2025-05-26 14:24:55 +0900

    Update exproblem_set1.js

--- exproblem_set1.js
+++ exproblem_set1.js
@@ -196,16 +196,16 @@ window.onload = () => {
 
   document.getElementById("submit-btn").onclick = confirmAndFinish;
 // モーダル拡大表示用の処理
-document.getElementById("quiz-img").addEventListener("click", function () {
-  const modal = document.getElementById("imageModal");
-  const modalImg = document.getElementById("modalImage");
-  modal.style.display = "block";
-  modalImg.src = this.src;
-});
-
-document.getElementById("imageModal").addEventListener("click", function () {
-  this.style.display = "none";
-});
+  document.getElementById("quiz-img").addEventListener("click", function () {
+    const modal = document.getElementById("imageModal");
+    const modalImg = document.getElementById("modalImage");
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  });
+  
+  document.getElementById("imageModal").addEventListener("click", function () {
+    this.style.display = "none";
+  });
   document.getElementById("confirm-yes").onclick = finishExam;
   document.getElementById("confirm-no").onclick = () => {
     document.getElementById("confirm-overlay").style.display = "none";
