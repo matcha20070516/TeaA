74052cbca9e588724b790697f27068884009a43c
matcha20070516 <ma0516_hogreen@icloud.com>
2025-06-11 20:48:15 +0900

    Update exresult.js

--- exresult.js
+++ exresult.js
@@ -6,7 +6,6 @@ window.addEventListener("DOMContentLoaded", () => {
   const score = localStorage.getItem(`${prefix}Score`) || localStorage.getItem("exScore") || "0";
   const displaySetName = localStorage.getItem(`${prefix}SetName`) || setName;
   const attemptCount = localStorage.getItem(`${prefix}AttemptCount`) || "1";
-  const elapsedTime = localStorage.getItem(`${prefix}ElapsedTime`) || "0";
 
   document.getElementById("username").textContent = username;
   document.getElementById("score").textContent = score;
