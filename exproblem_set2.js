ba9d8a841bb03d3fb56bbda53f6132b2680c32d7
matcha20070516 <ma0516_hogreen@icloud.com>
2025-10-22 10:10:41 +0900

    Update exproblem_set2.js

--- exproblem_set2.js
+++ exproblem_set2.js
@@ -138,6 +138,11 @@ const loadQuestion = () => {
   document.getElementById("quiz-img").src = `mq2${current}.PNG`;
   document.getElementById("answer").value = answers[current - 1] || "";
 
+  const pointSpan = document.getElementById("question-point");
+  if (pointSpan) {
+    pointSpan.textContent = pointsPerQuestion[current - 1] + "点";
+  }
+
   const formatSpan = document.getElementById("answer-format");
   formatSpan.textContent = answerFormats[current - 1] || "";
 
